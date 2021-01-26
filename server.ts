e8b15ca5c7dd01d9b20186040fb8754180b8082a
Gabriel Rodrigues <gabriel.brutu@gmail.com>
2021-01-26 15:22:09 -0300

    ajustado db

---
+++ server.ts
@@ -0,0 +1,12 @@
+import * as jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("JSON Server is running");
+});
